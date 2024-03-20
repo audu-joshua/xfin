46aba20bbe48e61c34535e6a0d84d18c8b3dc01a
Audu Joshua Adinoyi <empire4josh@gmail.com>
2024-03-20 12:18:39 +0100

    small change

--- components/homeComponents/blogs/indes.tsx
+++ components/homeComponents/blogs/indes.tsx
@@ -7,8 +7,8 @@ export default function Blogs() {
     <h2 className='text-[#FB2B30] text-center py-4 md:text-center md:py-10 text-3xl md:text-4xl lg:text-5xl font-bold'>Visit Our Blogs</h2>
     <div className='grid md:grid-cols-2 md:gap-4 lg:grid-cols-3'>
         {data.map((d, index) => ( // Added index parameter to map function
-            <div key={index} className='w-full lg:w-full items-center'> {/* Added key prop */}
-                <img src={d.image} alt={d.title} className=' lg:h-72'/>
+            <div key={index} className='w-full items-center'> {/* Added key prop */}
+                <img src={d.image} alt={d.title} />
                 <h3 className='text-black font-bold text-base md:text-xl lg:text-2xl py-4'>{d.title}</h3>
                 <p className='font-medium text-base text-black pb-8'>{d.date}</p>
             </div>
