217fd3c6717bbb837a3f5ad8183798c8c5c78e90
Audu Joshua Adinoyi <empire4josh@gmail.com>
2024-03-20 12:14:53 +0100

    Made Slight changes to Home Page

--- components/homeComponents/blogs/indes.tsx
+++ components/homeComponents/blogs/indes.tsx
@@ -3,14 +3,14 @@ import { isDate } from 'util/types'
 
 export default function Blogs() {
   return (
-    <div className='bg-white flex justify-center py-8'>
-    <h2>Visit Our Blog</h2>
+    <div className='bg-white justify-center py-8 lg:px-28 px-6'>
+    <h2 className='text-[#FB2B30] text-center py-4 md:text-center md:py-10 text-3xl md:text-4xl lg:text-5xl font-bold'>Visit Our Blogs</h2>
     <div className='grid md:grid-cols-2 md:gap-4 lg:grid-cols-3'>
         {data.map((d, index) => ( // Added index parameter to map function
-            <div key={index} className='w-[90%] lg:w-full'> {/* Added key prop */}
-                <img src={d.image} alt={d.title} className=''/>
+            <div key={index} className='w-full lg:w-full items-center'> {/* Added key prop */}
+                <img src={d.image} alt={d.title} className=' lg:h-72'/>
                 <h3 className='text-black font-bold text-base md:text-xl lg:text-2xl py-4'>{d.title}</h3>
-                <p className='font-medium text-base text-black'>{d.date}</p>
+                <p className='font-medium text-base text-black pb-8'>{d.date}</p>
             </div>
         ))}
     </div>
--- components/homeComponents/hero/index.tsx
+++ components/homeComponents/hero/index.tsx
@@ -36,12 +36,12 @@ export default function Hero() {
     <div className=' z-30 py-40 px-[20px] md:px-[30px] xl:px-[97px]' 
     style={isLargeScreen ? style.bgimageDesktop : style.bgimageMobile}
     >
-      <div>
+      <div className='text-white'>
            <h1 className=' text-4xl md:text-6xl lg:text-7xl font-bold py-4'> Website Design </h1>    
            <p className=' text-sm md:text-lg lg:text-2xl md:w-[60%] lg:w-[70%] font-normal'>Improve your brand recognition with appealing and interactive website design to attract the right prospects to your business.</p>    
 
            <div className='my-8'>
-                <p className='py-2 px-8 lg:py-3 bg-[#FF0909] text-sm md:text-lg lg:text-2xl font-normal rounded-full w-fit text-center justify-center'> Let’s Talk </p>
+                <p className='py-2 px-8 lg:py-3 text-white bg-[#FF0909] text-sm md:text-lg lg:text-2xl font-normal rounded-full w-fit text-center justify-center'> Let’s Talk </p>
            </div>
       </div>
     </div>
