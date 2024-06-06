4aeb60bf6a1d153fd3de75f185d8de6a285edff9
Audu Joshua Adinoyi <empire4josh@gmail.com>
2024-06-06 18:57:12 +0100

    made some changes

--- components/homeComponents/blogs/indes.tsx
+++ components/homeComponents/blogs/indes.tsx
@@ -1,47 +1,42 @@
-import React from 'react'
-import { isDate } from 'util/types'
-import b1 from '@/public/images/b1.png'
-import b2 from '@/public/images/b2.png'
-import b3 from '@/public/images/b3.png'
+import React from 'react';
+import b1 from '@/public/images/b1.png';
+import b2 from '@/public/images/b2.png';
+import b3 from '@/public/images/b3.png';
+import Image from 'next/image';
 
 export default function Blogs() {
   return (
     <div className='bg-white justify-center py-8 lg:px-28 px-6'>
-    <h2 className='text-[#FB2B30] text-center py-4 md:text-center md:py-10 text-3xl md:text-4xl lg:text-5xl font-bold'>Visit Our Blogs</h2>
-    <div className='grid md:grid-cols-2 md:gap-4 lg:grid-cols-3'>
-        {data.map((d, index) => ( // Added index parameter to map function
-            <div key={index} className='w-full items-center'> {/* Added key prop */}
-                <img src={d.image} alt={d.title} />
-                <h3 className='text-black font-bold text-base md:text-xl lg:text-2xl py-4'>{d.title}</h3>
-                <p className='font-medium text-base text-black pb-8'>{d.date}</p>
-            </div>
-        ))}
+      <h2 className='text-[#FB2B30] text-center py-4 md:text-center md:py-10 text-3xl md:text-4xl lg:text-5xl font-bold'>
+        Visit Our Blogs
+      </h2>
+      <div className='grid md:grid-cols-2 md:gap-4 lg:grid-cols-3'>
+
+        <div className='w-full items-center'>
+          <Image src={b1} alt="The Evolution Of Finance: A Journey From The Past To The Present" />
+          <h3 className='text-black font-bold text-base md:text-xl lg:text-2xl py-4'>
+            The Evolution Of Finance: A Journey From The Past To The Present
+          </h3>
+          <p className='font-medium text-base text-black pb-8'>2024-01-29</p>
+        </div>
+
+        <div className='w-full items-center'>
+          <Image src={b2} alt="The Evolution Of Finance: A Journey From The Past To The Present" />
+          <h3 className='text-black font-bold text-base md:text-xl lg:text-2xl py-4'>
+            The Evolution Of Finance: A Journey From The Past To The Present
+          </h3>
+          <p className='font-medium text-base text-black pb-8'>2024-01-29</p>
+        </div>
+
+        <div className='w-full items-center'>
+          <Image src={b3} alt="The Evolution Of Finance: A Journey From The Past To The Present" />
+          <h3 className='text-black font-bold text-base md:text-xl lg:text-2xl py-4'>
+            The Evolution Of Finance: A Journey From The Past To The Present
+          </h3>
+          <p className='font-medium text-base text-black pb-8'>2024-01-29</p>
+        </div>
+
+      </div>
     </div>
-</div>
-
   )
-}
-
-const data = [
-    {
-      id: 1,
-      image: {b1},
-      title: "The Evolution Of Finance: A Journey From The Past To The Present",
-      date: "2024-01-29"
-    },
-  
-    {
-      id: 2,
-      image: {b2},
-      title: "The Evolution Of Finance: A Journey From The Past To The Present",
-      date: "2024-01-29"
-    },
-  
-    {
-      id: 3,
-      image: {b3},
-      title: "The Evolution Of Finance: A Journey From The Past To The Present",
-      date: "2024-01-29"
-    },
-  
-  ]
+};
