91c0860db50b70e2a796f11eb5f608408825ac37
Audu Joshua Adinoyi <empire4josh@gmail.com>
2024-08-26 09:14:09 +0100

    Updated the Benefits Section Again

--- components/serviceComponents/benefits/index.tsx
+++ components/serviceComponents/benefits/index.tsx
@@ -39,7 +39,7 @@ export function Benefits() {
   return (
     <div className='bg-[rgb(227,225,225)] lg:px-28 px-6 py-6 overflow-hidden'>
         <div className=' md:bg-[#252424] px-2 md:px-4 lg:px-6 rounded-3xl'>
-            <h1 className='md:text-white text-[#252424] md:pt-4 text-4xl md:text-4xl lg:text-6xl'> Benefits Of Working With Xifin Enterprise </h1>
+            <h1 className='md:text-white text-[#252424] md:pt-4 text-3xl md:text-4xl lg:text-6xl'> Benefits Of Working With Xifin Enterprise </h1>
             <p className=' text-black py-8 md:py-4 md:text-white lg:w-[70%]'> Combining attractive design and product utility, we create numerous benefits with our digital product experience design services.</p>
 
             <div className='grid lg:py-12 md:grid-cols-2 gap-2 md:gap-10 md:justify-end md:items-end lg:gap-12'>
